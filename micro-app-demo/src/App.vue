cb0e83be6bbbe65587a072fba41367d9f8605720
yangyi152 <yangyi152@gdii-yueyun.com>
2025-03-18 11:37:21 +0800

    feat: 改为用微前端的push

--- micro-app-demo/src/App.vue
+++ micro-app-demo/src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { useRouter } from "vue-router"
+import microApp from '@micro-zoe/micro-app';
+
+const router = useRouter()
+const onClick = (path) => {
+  // router.push('/microOtp/home')
+  // router.push('/microOtp/about')
+  microApp.router.push({ name: 'microOtp', path: `/#/microOtp/${path}` });
+}
 </script>
 
 <template>
-    <router-link to="/microOtp/home">Home</router-link> |
-    <router-link to="/microOtp/about">About</router-link>
+    <!-- <router-link to="/microOtp/home">Home</router-link> | -->
+    <!-- <router-link to="/microOtp/about">About</router-link> -->
+    <div>
+      <span @click="onClick('home')">Home</span> /
+      <span @click="onClick('about')">About</span>
+    </div>
     <router-view v-slot="{ Component, route }">
       <component :is="Component" :key="route.name" />
     </router-view>
